1deef51d011461a57e4a80c3b864ec57ba7309f5
David Tweedle <david.aig.tweedle@gmail.com>
2025-12-20 13:56:24 -0500

    more testing

--- src/gptq-svd/gptq_utils.py
+++ src/gptq-svd/gptq_utils.py
@@ -254,8 +254,8 @@ if __name__ == '__main__':
     torch.manual_seed(0)
 
     num_samples = 1024 * 32
-    n = 2048
-    m = 2048
+    n = 1024
+    m = 1024
     device = torch.device("cuda")
     dtype = torch.float32
     # X = torch.randn(num_samples, n, device=device, dtype=dtype)
@@ -278,14 +278,14 @@ if __name__ == '__main__':
         q = Quantizer(per_channel=True, w_bits=4)
 
         gptq_svd_fwrd(
-                sketch_dim=4 * n,
-                oversample=64,
+                sketch_dim=32,
+                oversample=32,
                 k_iter=5,
                 make_stream=make_stream,
                 weight_mat=weight_mat,
                 out_weight=out_weight,
                 quantizer=q,
-                eps=1e-5
+                eps=1e-8
                 )
         Y_quant_svd = X @ out_weight.T
 
